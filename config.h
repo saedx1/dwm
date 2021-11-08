3715c465cf5ac75b3335a5a6344272a3e2efb874
saedx1 <34514167+saedx1@users.noreply.github.com>
2021-11-07 20:46:48 -0500

    update dwm

--- config.h
+++ config.h
@@ -1,27 +1,40 @@
 /* See LICENSE file for copyright and license details. */
+#include "layouts.c"
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 10;        /* gap pixel between windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const int user_bh            = 10;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static char *fonts[]          = { "Calibri:pixelsize=20:antialias=true:autohint=true" , "JoyPixels:pixelsize=16:antialias=true:autohint=true", "Noto Sans Arabic UI:pixelsize=20:antialias=true:autohint=true"  };
+static const unsigned int borderpx = 1; /* border pixel of windows */
+static const unsigned int gappx = 10;	/* gap pixel between windows */
+static const unsigned int snap = 32;	/* snap pixel */
+static const int user_bh = 10;			/* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
+static const int showbar = 1;			/* 0 means no bar */
+static const int topbar = 1;			/* 0 means bottom bar */
+static char *fonts[] = {"Calibri:pixelsize=18:antialias=true:autohint=true", "JoyPixels:pixelsize=16:antialias=true:autohint=true", "Noto Sans Arabic UI:pixelsize=20:antialias=true:autohint=true"};
 // static char *fonts[]          		= { "Calibri:pixelsize=20:antialias=true:autohint=true"    };
 // static char *fonts[]          		= {"Noto Color Emoji:pixelsize=18:antialias=true:autohint=true", "Noto Sans Display:pixelsize=18:antialias=true:autohint=true"    };
-static const char dmenufont[]       = "JetBrains Mono:pixelsize=16:antialias=true:autohint=true";
-static const char colprimary[]       = "#1abc9c";
-static const char colsecondary[]       = "#2c3e50";
-static const char colborders[]       = "#8db5cf";
-static const char *colors[][3]      = {
+static const char dmenufont[] = "JetBrains Mono:pixelsize=16:antialias=true:autohint=true";
+static const char colprimary[] = "#1abc9c";
+static const char colsecondary[] = "#2c3e50";
+static const char colborders[] = "#8db5cf";
+static const char *colors[][3] = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { colprimary, colsecondary, colborders },
-	[SchemeSel]  = { colsecondary, colprimary, colborders },
+	[SchemeNorm] = {colprimary, colsecondary, colborders},
+	[SchemeSel] = {colsecondary, colprimary, colborders},
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+
+static const char *tagsel[][2] = {
+	{"#ffffff", "#ff0000"},
+	{"#ffffff", "#ff7f00"},
+	{"#000000", "#ffff00"},
+	{"#000000", "#00ff00"},
+	{"#ffffff", "#0000ff"},
+	{"#ffffff", "#4b0082"},
+	{"#ffffff", "#9400d3"},
+	{"#000000", "#ffffff"},
+	{"#ffffff", "#000000"},
+};
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -29,83 +42,87 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     					NULL,       NULL,       0,            1,           -1 },
-	{ "Microsoft Teams - Preview",  NULL,       NULL,       1 << 3,       0,           -1 },
-	{ "Slack",  					NULL,       NULL,       1 << 3,       0,           -1 },
+	{"Gimp", NULL, NULL, 0, 1, -1},
+	{"Microsoft Teams - Preview", NULL, NULL, 1 << 3, 0, -1},
+	{"Slack", NULL, NULL, 1 << 3, 0, -1},
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
-static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const float mfact = 0.55;  /* factor of master area size [0.05..0.95] */
+static const int nmaster = 1;	  /* number of clients in master area */
+static const int resizehints = 1; /* 1 means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[T]",      tile },    /* first entry is default */
-	{ "[F]",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
+	{"[T]", tile}, /* first entry is default */
+	{"[F]", NULL}, /* no layout function means floating behavior */
+	{"[M]", monocle},
+	{"HHH", grid},
 };
 
 /* key definitions */
 #define ALTMASK Mod1Mask
 #define MODKEY Mod4Mask
-#define TAGKEYS(KEY,TAG) \
-	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
-	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
+#define TAGKEYS(KEY, TAG)                                          \
+	{MODKEY, KEY, view, {.ui = 1 << TAG}},                         \
+		{MODKEY | ControlMask, KEY, toggleview, {.ui = 1 << TAG}}, \
+		{MODKEY | ShiftMask, KEY, tag, {.ui = 1 << TAG}},          \
+		{MODKEY | ControlMask | ShiftMask, KEY, toggletag, {.ui = 1 << TAG}},
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
-#define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
+#define SHCMD(cmd)                                           \
+	{                                                        \
+		.v = (const char *[]) { "/bin/sh", "-c", cmd, NULL } \
+	}
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] =			{ "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", colsecondary, "-nf", colprimary, "-sb", colprimary, "-sf", colsecondary, NULL };
-static const char *termcmd[]  = 		{ "st", NULL };
-static const char *cmdprintscreen[]  = 	{ "scrot", "-sf", "/home/saedx1/screenshots/%Y-%m-%d-%s_$wx$h.png", NULL };
+static const char *dmenucmd[] = {"dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", colsecondary, "-nf", colprimary, "-sb", colprimary, "-sf", colsecondary, NULL};
+static const char *termcmd[] = {"st", NULL};
+static const char *cmdprintscreen[] = {"scrot", "-sf", "/home/saedx1/screenshots/%Y-%m-%d-%s_$wx$h.png", NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,						XK_p,		spawn,          	{.v = dmenucmd } },
-	{ MODKEY,						XK_space, 	spawn,          	{.v = termcmd } },
-	{ MODKEY,                       XK_b,      	togglebar,      	{0} },
-	{ MODKEY,						XK_Up,		spawn,     			SHCMD("pulseaudio-ctl up") },
-	{ MODKEY,						XK_Down,  	spawn,     			SHCMD("pulseaudio-ctl down") },
-	{ 0,							XK_Print,  	spawn,				{.v = cmdprintscreen } },
-	{ MODKEY,						XK_Right,  	focusstack,     	{.i = +1 } },
-	{ MODKEY,						XK_Left,   	focusstack,     	{.i = -1 } },
-	{ MODKEY,                       XK_i,      	incnmaster,     	{.i = +1 } },
-	{ MODKEY,                       XK_d,      	incnmaster,     	{.i = -1 } },
-	{ MODKEY,                       XK_h,      	setmfact,       	{.f = -0.05} },
-	{ MODKEY,                       XK_l,      	setmfact,       	{.f = +0.05} },
-	{ MODKEY,                       XK_Return, 	zoom,           	{0} },
-	{ MODKEY,                       XK_Tab,    	view,           	{0} },
-	{ MODKEY|ShiftMask,             XK_c,      	killclient,     	{0} },
-	{ MODKEY,                       XK_t,   	setlayout,      	{.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      	setlayout,      	{.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      	setlayout,      	{.v = &layouts[2]} },
-	{ 0, 							0,  		setlayout,      	{0} },
-	{ MODKEY|ShiftMask,             XK_space,  	togglefloating, 	{0} },
-	{ MODKEY,                       XK_0,      	view,           	{.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      	tag,            	{.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  	focusmon,       	{.i = -1 } },
-	{ MODKEY,                       XK_period, 	focusmon,       	{.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  	tagmon,         	{.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, 	tagmon,         	{.i = +1 } },
-	{ MODKEY,             			XK_n,		spawn,				SHCMD("$BROWSER") },
-	{ ALTMASK,             			XK_Shift_L,	spawn,				SHCMD("language_switch") },
-	{ ShiftMask,             		XK_Alt_L,	spawn,				SHCMD("language_switch") },
-	{ MODKEY, 						XK_s,		spawn,				SHCMD("sleep_pc") },
-	TAGKEYS(                        XK_1,                      		0)
-	TAGKEYS(                        XK_2,                      		1)
-	TAGKEYS(                        XK_3,                      		2)
-	TAGKEYS(                        XK_4,                      		3)
-	TAGKEYS(                        XK_5,                      		4)
-	TAGKEYS(                        XK_6,                      		5)
-	TAGKEYS(                        XK_7,                      		6)
-	TAGKEYS(                        XK_8,                      		7)
-	TAGKEYS(                        XK_9,                      		8)
-	{ MODKEY|ShiftMask,             XK_q,     	 quit,           	{0} }
+	{MODKEY, XK_p, spawn, {.v = dmenucmd}},
+	{MODKEY, XK_space, spawn, {.v = termcmd}},
+	{MODKEY, XK_b, togglebar, {0}},
+	{MODKEY, XK_Up, spawn, SHCMD("pulseaudio-ctl up")},
+	{MODKEY, XK_Down, spawn, SHCMD("pulseaudio-ctl down")},
+	{0, XK_Print, spawn, {.v = cmdprintscreen}},
+	{MODKEY, XK_Right, focusstack, {.i = +1}},
+	{MODKEY, XK_Left, focusstack, {.i = -1}},
+	{MODKEY, XK_i, incnmaster, {.i = +1}},
+	{MODKEY, XK_d, incnmaster, {.i = -1}},
+	{MODKEY, XK_h, setmfact, {.f = -0.05}},
+	{MODKEY, XK_l, setmfact, {.f = +0.05}},
+	{MODKEY, XK_Return, zoom, {0}},
+	{MODKEY, XK_Tab, view, {0}},
+	{MODKEY | ShiftMask, XK_c, killclient, {0}},
+	{MODKEY, XK_t, setlayout, {.v = &layouts[0]}},
+	{MODKEY, XK_f, setlayout, {.v = &layouts[1]}},
+	{MODKEY, XK_m, setlayout, {.v = &layouts[2]}},
+	{MODKEY, XK_g, setlayout, {.v = &layouts[3]}},
+	{0, 0, setlayout, {0}},
+	{MODKEY | ShiftMask, XK_space, togglefloating, {0}},
+	{MODKEY, XK_0, view, {.ui = ~0}},
+	{MODKEY | ShiftMask, XK_0, tag, {.ui = ~0}},
+	{MODKEY, XK_comma, focusmon, {.i = -1}},
+	{MODKEY, XK_period, focusmon, {.i = +1}},
+	{MODKEY | ShiftMask, XK_comma, tagmon, {.i = -1}},
+	{MODKEY | ShiftMask, XK_period, tagmon, {.i = +1}},
+	{MODKEY, XK_n, spawn, SHCMD("$BROWSER")},
+	{ALTMASK, XK_Shift_L, spawn, SHCMD("language_switch")},
+	{ShiftMask, XK_Alt_L, spawn, SHCMD("language_switch")},
+	{MODKEY, XK_s, spawn, SHCMD("sleep_pc")},
+	TAGKEYS(XK_1, 0)
+		TAGKEYS(XK_2, 1)
+			TAGKEYS(XK_3, 2)
+				TAGKEYS(XK_4, 3)
+					TAGKEYS(XK_5, 4)
+						TAGKEYS(XK_6, 5)
+							TAGKEYS(XK_7, 6)
+								TAGKEYS(XK_8, 7)
+									TAGKEYS(XK_9, 8){MODKEY | ShiftMask, XK_q, quit, {0}}
 
 };
 
@@ -113,18 +130,17 @@ static Key keys[] = {
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
 	/* click                event mask      button          function        argument */
-	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
-	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
-	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
-	{ ClkStatusText,        0,              Button1,        spawn,          SHCMD("st -e htop")},
-	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
-	{ ClkStatusText,        0,              Button3,        spawn,			SHCMD("killall activate_status_bar && activate_status_bar &") },
-	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
-	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
-	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
-	{ ClkTagBar,            0,              Button1,        view,           {0} },
-	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
-	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
-	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+	{ClkLtSymbol, 0, Button1, setlayout, {0}},
+	{ClkLtSymbol, 0, Button3, setlayout, {.v = &layouts[2]}},
+	{ClkWinTitle, 0, Button2, zoom, {0}},
+	{ClkStatusText, 0, Button1, spawn, SHCMD("st -e htop")},
+	{ClkStatusText, 0, Button2, spawn, {.v = termcmd}},
+	{ClkStatusText, 0, Button3, spawn, SHCMD("killall activate_status_bar && activate_status_bar &")},
+	{ClkClientWin, MODKEY, Button1, movemouse, {0}},
+	{ClkClientWin, MODKEY, Button2, togglefloating, {0}},
+	{ClkClientWin, MODKEY, Button3, resizemouse, {0}},
+	{ClkTagBar, 0, Button1, view, {0}},
+	{ClkTagBar, 0, Button3, toggleview, {0}},
+	{ClkTagBar, MODKEY, Button1, tag, {0}},
+	{ClkTagBar, MODKEY, Button3, toggletag, {0}},
 };
-
--- dwm.c
+++ dwm.c
@@ -112,6 +112,7 @@ typedef struct {
 	void (*arrange)(Monitor *);
 } Layout;
 
+typedef struct Pertag Pertag;
 struct Monitor {
 	char ltsymbol[16];
 	float mfact;
@@ -131,6 +132,7 @@ struct Monitor {
 	Monitor *next;
 	Window barwin;
 	const Layout *lt[2];
+	Pertag *pertag;
 };
 
 typedef struct {
@@ -270,6 +272,7 @@ static Atom wmatom[WMLast], netatom[NetLast];
 static int running = 1;
 static Cur *cursor[CurLast];
 static Clr **scheme;
+static Clr **tagscheme;
 static Display *dpy;
 static Drw *drw;
 static Monitor *mons, *selmon;
@@ -277,6 +280,15 @@ static Window root, wmcheckwin;
 
 /* configuration, allows nested code to access above variables */
 #include "config.h"
+
+struct Pertag {
+	unsigned int curtag, prevtag; /* current and previous tag */
+	int nmasters[LENGTH(tags) + 1]; /* number of windows in master area */
+	float mfacts[LENGTH(tags) + 1]; /* mfacts per tag */
+	unsigned int sellts[LENGTH(tags) + 1]; /* selected layouts */
+	const Layout *ltidxs[LENGTH(tags) + 1][2]; /* matrix of tags and layouts indexes  */
+	int showbars[LENGTH(tags) + 1]; /* display bar for the current tag */
+};
 
 /* compile-time check if all tags fit into an unsigned int bit array. */
 struct NumTags { char limitexceeded[LENGTH(tags) > 31 ? -1 : 1]; };
@@ -438,9 +450,15 @@ buttonpress(XEvent *e)
 	}
 	if (ev->window == selmon->barwin) {
 		i = x = 0;
-		do
-			x += TEXTW(tags[i]);
-		while (ev->x >= x && ++i < LENGTH(tags));
+		unsigned int occ = 0;
+		for(c = m->clients; c; c = c->next)
+			occ |= c->tags;
+		do {
+			/* do not reserve space for vacant tags */
+			if(!(occ & 1 << i || m->tagset[m->seltags] & 1 << i))
+				continue;
+ 			x += TEXTW(tags[i]);
+		} while (ev->x >= x && ++i < LENGTH(tags));
 		if (i < LENGTH(tags)) {
 			click = ClkTagBar;
 			arg.ui = 1 << i;
@@ -638,6 +656,7 @@ Monitor *
 createmon(void)
 {
 	Monitor *m;
+	unsigned int i;
 
 	m = ecalloc(1, sizeof(Monitor));
 	m->tagset[0] = m->tagset[1] = 1;
@@ -648,6 +667,20 @@ createmon(void)
 	m->lt[0] = &layouts[0];
 	m->lt[1] = &layouts[1 % LENGTH(layouts)];
 	strncpy(m->ltsymbol, layouts[0].symbol, sizeof m->ltsymbol);
+	m->pertag = ecalloc(1, sizeof(Pertag));
+	m->pertag->curtag = m->pertag->prevtag = 1;
+
+	for (i = 0; i <= LENGTH(tags); i++) {
+		m->pertag->nmasters[i] = m->nmaster;
+		m->pertag->mfacts[i] = m->mfact;
+
+		m->pertag->ltidxs[i][0] = m->lt[0];
+		m->pertag->ltidxs[i][1] = m->lt[1];
+		m->pertag->sellts[i] = m->sellt;
+
+		m->pertag->showbars[i] = m->showbar;
+	}
+
 	return m;
 }
 
@@ -722,8 +755,12 @@ drawbar(Monitor *m)
 	}
 	x = 0;
 	for (i = 0; i < LENGTH(tags); i++) {
+		/* do not draw vacant tags */
+		if(!(occ & 1 << i || m->tagset[m->seltags] & 1 << i))
+			continue;
 		w = TEXTW(tags[i]);
-		drw_setscheme(drw, scheme[m->tagset[m->seltags] & 1 << i ? SchemeSel : SchemeNorm]);
+		// drw_setscheme(drw, scheme[m->tagset[m->seltags] & 1 << i ? SchemeSel : SchemeNorm]);
+		drw_setscheme(drw, (m->tagset[m->seltags] & 1 << i ? tagscheme[i] : scheme[SchemeNorm]));
 		drw_text(drw, x, 0, w, bh, lrpad / 2, tags[i], urg & 1 << i);
 		if (occ & 1 << i)
 			drw_rect(drw, x + boxs, boxs, boxw, boxw,
@@ -973,7 +1010,7 @@ grabkeys(void)
 void
 incnmaster(const Arg *arg)
 {
-	selmon->nmaster = MAX(selmon->nmaster + arg->i, 0);
+	selmon->nmaster = selmon->pertag->nmasters[selmon->pertag->curtag] = MAX(selmon->nmaster + arg->i, 0);
 	arrange(selmon);
 }
 
@@ -1591,9 +1628,9 @@ void
 setlayout(const Arg *arg)
 {
 	if (!arg || !arg->v || arg->v != selmon->lt[selmon->sellt])
-		selmon->sellt ^= 1;
+		selmon->sellt = selmon->pertag->sellts[selmon->pertag->curtag] ^= 1;
 	if (arg && arg->v)
-		selmon->lt[selmon->sellt] = (Layout *)arg->v;
+		selmon->lt[selmon->sellt] = selmon->pertag->ltidxs[selmon->pertag->curtag][selmon->sellt] = (Layout *)arg->v;
 	strncpy(selmon->ltsymbol, selmon->lt[selmon->sellt]->symbol, sizeof selmon->ltsymbol);
 	if (selmon->sel)
 		arrange(selmon);
@@ -1612,7 +1649,7 @@ setmfact(const Arg *arg)
 	f = arg->f < 1.0 ? arg->f + selmon->mfact : arg->f - 1.0;
 	if (f < 0.05 || f > 0.95)
 		return;
-	selmon->mfact = f;
+	selmon->mfact = selmon->pertag->mfacts[selmon->pertag->curtag] = f;
 	arrange(selmon);
 }
 
@@ -1657,9 +1694,14 @@ setup(void)
 	cursor[CurResize] = drw_cur_create(drw, XC_sizing);
 	cursor[CurMove] = drw_cur_create(drw, XC_fleur);
 	/* init appearance */
+	if (LENGTH(tags) > LENGTH(tagsel))
+		die("too few color schemes for the tags");
 	scheme = ecalloc(LENGTH(colors), sizeof(Clr *));
 	for (i = 0; i < LENGTH(colors); i++)
 		scheme[i] = drw_scm_create(drw, colors[i], 3);
+	tagscheme = ecalloc(LENGTH(tagsel), sizeof(Clr *));
+	for (i = 0; i < LENGTH(tagsel); i++)
+		tagscheme[i] = drw_scm_create(drw, tagsel[i], 2);
 	/* init bars */
 	updatebars();
 	updatestatus();
@@ -1793,7 +1835,7 @@ tile(Monitor *m)
 void
 togglebar(const Arg *arg)
 {
-	selmon->showbar = !selmon->showbar;
+	selmon->showbar = selmon->pertag->showbars[selmon->pertag->curtag] = !selmon->showbar;
 	updatebarpos(selmon);
 	XMoveResizeWindow(dpy, selmon->barwin, selmon->wx, selmon->by, selmon->ww, bh);
 	arrange(selmon);
@@ -1832,9 +1874,33 @@ void
 toggleview(const Arg *arg)
 {
 	unsigned int newtagset = selmon->tagset[selmon->seltags] ^ (arg->ui & TAGMASK);
+	int i;
 
 	if (newtagset) {
 		selmon->tagset[selmon->seltags] = newtagset;
+
+		if (newtagset == ~0) {
+			selmon->pertag->prevtag = selmon->pertag->curtag;
+			selmon->pertag->curtag = 0;
+		}
+
+		/* test if the user did not select the same tag */
+		if (!(newtagset & 1 << (selmon->pertag->curtag - 1))) {
+			selmon->pertag->prevtag = selmon->pertag->curtag;
+			for (i = 0; !(newtagset & 1 << i); i++) ;
+			selmon->pertag->curtag = i + 1;
+		}
+
+		/* apply settings for this view */
+		selmon->nmaster = selmon->pertag->nmasters[selmon->pertag->curtag];
+		selmon->mfact = selmon->pertag->mfacts[selmon->pertag->curtag];
+		selmon->sellt = selmon->pertag->sellts[selmon->pertag->curtag];
+		selmon->lt[selmon->sellt] = selmon->pertag->ltidxs[selmon->pertag->curtag][selmon->sellt];
+		selmon->lt[selmon->sellt^1] = selmon->pertag->ltidxs[selmon->pertag->curtag][selmon->sellt^1];
+
+		if (selmon->showbar != selmon->pertag->showbars[selmon->pertag->curtag])
+			togglebar(NULL);
+
 		focus(NULL);
 		arrange(selmon);
 	}
@@ -2135,11 +2201,37 @@ updatewmhints(Client *c)
 void
 view(const Arg *arg)
 {
+	int i;
+	unsigned int tmptag;
+
 	if ((arg->ui & TAGMASK) == selmon->tagset[selmon->seltags])
 		return;
 	selmon->seltags ^= 1; /* toggle sel tagset */
-	if (arg->ui & TAGMASK)
+	if (arg->ui & TAGMASK) {
 		selmon->tagset[selmon->seltags] = arg->ui & TAGMASK;
+		selmon->pertag->prevtag = selmon->pertag->curtag;
+
+		if (arg->ui == ~0)
+			selmon->pertag->curtag = 0;
+		else {
+			for (i = 0; !(arg->ui & 1 << i); i++) ;
+			selmon->pertag->curtag = i + 1;
+		}
+	} else {
+		tmptag = selmon->pertag->prevtag;
+		selmon->pertag->prevtag = selmon->pertag->curtag;
+		selmon->pertag->curtag = tmptag;
+	}
+
+	selmon->nmaster = selmon->pertag->nmasters[selmon->pertag->curtag];
+	selmon->mfact = selmon->pertag->mfacts[selmon->pertag->curtag];
+	selmon->sellt = selmon->pertag->sellts[selmon->pertag->curtag];
+	selmon->lt[selmon->sellt] = selmon->pertag->ltidxs[selmon->pertag->curtag][selmon->sellt];
+	selmon->lt[selmon->sellt^1] = selmon->pertag->ltidxs[selmon->pertag->curtag][selmon->sellt^1];
+
+	if (selmon->showbar != selmon->pertag->showbars[selmon->pertag->curtag])
+		togglebar(NULL);
+
 	focus(NULL);
 	arrange(selmon);
 }
